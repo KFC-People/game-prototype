82603c75437a6a166c0fca6eea6d3a4600c8183e
Andrew Yatsura <andrewyazura203@proton.me>
2024-05-06 06:51:49 +0100

    add __all__ to views/__init__.py

--- client/views/__init__.py
+++ client/views/__init__.py
@@ -2,3 +2,10 @@ from client.views.game import GameView
 from client.views.main_menu import MainMenuView
 from client.views.options import OptionsView
 from client.views.splash import SplashView
+
+__all__ = [
+    "GameView",
+    "MainMenuView",
+    "OptionsView",
+    "SplashView",
+]
