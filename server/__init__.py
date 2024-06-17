00f5b5f73685b3f1e5a851e2daf230d5d044b583
Andrew Yatsura <andrewyazura203@proton.me>
2024-06-17 10:27:34 +0200

    use 0.0.0.0 on server by default

--- server/__init__.py
+++ server/__init__.py
@@ -2,4 +2,4 @@ from server.game_lobby import GameLobby
 
 
 def run(update_rate: float = 1 / 10) -> None:
-    GameLobby(host="127.0.0.1", port=1234, update_rate=update_rate).run()
+    GameLobby(host="0.0.0.0", port=1234, update_rate=update_rate).run()
