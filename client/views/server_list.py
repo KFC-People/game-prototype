86930de8993f4601de30fb8175f4ebb2d3787220
Andrew Yatsura <andrewyazura203@proton.me>
2024-06-17 17:40:47 +0200

    move to gcp

--- client/views/server_list.py
+++ client/views/server_list.py
@@ -21,16 +21,16 @@ class ServerListView(arcade.View):
         self.servers = self.get_servers()
 
     def get_servers(self) -> list[tuple[str, int]]:
-        return [("172.17.0.2", 1234)]
+        return [("gcp", "34.32.68.138", 1234)]
 
     def on_draw(self) -> None:
         self.clear()
 
         width, height = self.window.get_size()
 
-        for i, (host, port) in enumerate(self.servers, start=2):
+        for i, (name, host, port) in enumerate(self.servers, start=2):
             arcade.draw_text(
-                f"{i - 2} - {host}:{port}",
+                f"{i - 2} - {name}",
                 start_x=width / 2,
                 start_y=(10 - i) / 10 * height,
                 **self.font_config,
@@ -39,6 +39,6 @@ class ServerListView(arcade.View):
     def on_key_press(self, symbol: int, modifiers: int) -> None:
         if 0 <= (i := symbol - 48) < len(self.servers):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(self.servers[i])
+            s.connect(self.servers[i][1:])
 
             self.window.show_view(GameView(Connection(s)))
