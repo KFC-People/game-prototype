c0c309bcc7726483c5dc848afb21df25218cc747
Andrew Yatsura <andrewyazura203@proton.me>
2024-06-23 22:42:53 +0200

    add docker default ip

--- client/views/server_list.py
+++ client/views/server_list.py
@@ -21,7 +21,8 @@ class ServerListView(arcade.View):
         self.servers = self.get_servers()
 
     def get_servers(self) -> list[tuple[str, int]]:
-        return [("gcp", "34.32.68.138", 1234)]
+        return [("gcp", "172.17.0.2", 1234)]
+        # return [("gcp", "34.32.68.138", 1234)]
 
     def on_draw(self) -> None:
         self.clear()
